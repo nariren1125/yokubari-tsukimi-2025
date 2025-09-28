c8bd361d3c25465325796c2375c910c84fb36b48
成松廉 <ren1125.9872@gmail.com>
2025-09-28 18:04:19 +0900

    rspecの修正

--- spec/requests/items_spec.rb
+++ spec/requests/items_spec.rb
@@ -1,18 +1,29 @@
+# spec/requests/items_spec.rb
 require 'rails_helper'
 
 RSpec.describe "Items", type: :request do
   describe "GET /index" do
     it "returns http success" do
-      get "/items/index"
+      Item.create!(name: "テスト商品", desc: "説明", img: "test.png", maker: "メーカー", period: "期間", tweet_text: "#test", category: "meal")
+      get "/items"
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /show" do
     it "returns http success" do
-      get "/items/show"
+      item = Item.create!(
+        name: "テスト商品",
+        desc: "説明",
+        img: "test.png",
+        maker: "メーカー",
+        period: "期間",
+        tweet_text: "#test",
+        category: "meal"
+      )
+  
+      get "/items/#{item.id}.json"
       expect(response).to have_http_status(:success)
     end
   end
-
 end
