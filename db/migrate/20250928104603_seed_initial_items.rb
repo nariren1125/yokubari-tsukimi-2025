d73195dbe291b8d12a29cc567453222af8e40827
成松廉 <ren1125.9872@gmail.com>
2025-09-28 19:47:10 +0900

    Add migration to seed initial items

---
+++ db/migrate/20250928104603_seed_initial_items.rb
@@ -0,0 +1,42 @@
+class SeedInitialItems < ActiveRecord::Migration[7.0]
+  def up
+    Item.create!(
+      [
+        {
+          name: "月見バーガー",
+          desc: "秋の風物詩としてご好評いただいている定番の「月見バーガー」...",
+          img: "meal/meal_mac.png",
+          maker: "マクドナルド",
+          period: "2024年9月3日〜",
+          tweet_text: "#お月見限定商品を楽しもう #秋のミニアプリweek",
+          official_link: "https://www.mcdonalds.co.jp/campaign/tsukimi/",
+          category: "meal"
+        },
+        {
+          name: "月見スイーツ",
+          desc: "「月見パイ」が進化して登場...",
+          img: "dessert/dessert_mac.png",
+          maker: "マクドナルド",
+          period: "9月3日〜なくなり次第販売終了",
+          tweet_text: "#お月見限定商品を楽しもう #秋のミニアプリweek",
+          official_link: "https://www.mcdonalds.co.jp/company/news/2025/0825a/",
+          category: "dessert"
+        },
+        {
+          name: "BUNNY BRINGS JOY",
+          desc: "三日月を運ぶウサギたちの旅をテーマにデザインしました...",
+          img: "goods/goods_stb_tumbler.png",
+          maker: "スターバックス",
+          period: "9月3日〜なくなり次第終了",
+          tweet_text: "#お月見限定商品を楽しもう #秋のミニアプリweek",
+          official_link: "https://www.starbucks.co.jp/seasonal-goods/",
+          category: "goods"
+        }
+      ]
+    )
+  end
+
+  def down
+    Item.delete_all
+  end
+end
