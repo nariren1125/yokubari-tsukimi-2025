f737f9a0ace4573581f563c7305f5db8e82c3fdb
成松廉 <ren1125.9872@gmail.com>
2025-09-28 23:40:32 +0900

    OGP画像をpublic配下に移動・不要なmigration/controller/viewを削除

--- db/migrate/20250928075632_rename_image_url_to_img_in_items.rb
+++
@@ -1,5 +0,0 @@
-class RenameImageUrlToImgInItems < ActiveRecord::Migration[7.2]
-  def change
-    rename_column :items, :image_url, :img
-  end
-end
--- db/migrate/20250928080458_add_official_link_to_items.rb
+++
@@ -1,5 +0,0 @@
-class AddOfficialLinkToItems < ActiveRecord::Migration[7.2]
-  def change
-    add_column :items, :official_link, :string
-  end
-end
