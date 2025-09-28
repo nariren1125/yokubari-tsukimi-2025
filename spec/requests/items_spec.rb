e4d3e0359d8a2a6ff0c2de672a298be02f0faf3c
成松廉 <ren1125.9872@gmail.com>
2025-09-28 20:40:30 +0900

    rspecの修正

--- spec/requests/items_spec.rb
+++ spec/requests/items_spec.rb
@@ -1,10 +1,16 @@
 # spec/requests/items_spec.rb
-require 'rails_helper'
-
 RSpec.describe "Items", type: :request do
   describe "GET /index" do
     it "returns http success" do
-      Item.create!(name: "テスト商品", desc: "説明", img: "test.png", maker: "メーカー", period: "期間", tweet_text: "#test", category: "meal")
+      Item.create!(
+        name: "テスト商品",
+        desc: "説明",
+        img: "ogp_image.png",   # ← 存在する画像名に変更
+        maker: "メーカー",
+        period: "期間",
+        tweet_text: "#test",
+        category: "meal"
+      )
       get "/items"
       expect(response).to have_http_status(:success)
     end
@@ -15,7 +21,7 @@ RSpec.describe "Items", type: :request do
       item = Item.create!(
         name: "テスト商品",
         desc: "説明",
-        img: "test.png",
+        img: "ogp_image.png",   # ← こちらも変更
         maker: "メーカー",
         period: "期間",
         tweet_text: "#test",
