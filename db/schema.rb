2688177204d608d15e71667a4bf898ff2b253d4b
成松廉 <ren1125.9872@gmail.com>
2025-09-28 16:57:31 +0900

    DBの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_09_18_013432) do
+ActiveRecord::Schema[7.2].define(version: 2025_09_28_075632) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.text "desc"
+    t.string "img"
+    t.string "maker"
+    t.string "period"
+    t.string "tweet_text"
+    t.string "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,30 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Item.create!(
+  [
+    {
+      name: "月見バーガー",
+      desc: "秋の風物詩としてご好評いただいている定番の「月見バーガー」新商品5種を“月見ファミリー”として9月3日(水)より全国のマクドナルド店舗にて期間限定で販売いたします。",
+      img: "<%= image_path('meal/meal_mac.png') %>",
+      maker: "マクドナルド",
+      period: "2024年9月3日〜",
+      tweetText: "#お月見限定商品を楽しもう #秋のミニアプリweek",
+      officialLink: "https://www.mcdonalds.co.jp/campaign/tsukimi/"
+    },
+    {
+      name: "月見スイーツ",
+      desc: "「月見パイ」が進化して登場。つぶあんとやわらかいおもちに、新たにバターフィリングを加え、サクサクのパイで包んだ一品と、山梨県産シャインマスカットの果汁を使用したフルーティーなこの秋限定のマックシェイク。",
+      img: "<%= image_path('dessert/dessert_mac.png') %>",
+      maker: "マクドナルド",
+      period: "9月3日〜なくなり次第販売終了",
+      officialLink: "https://www.mcdonalds.co.jp/company/news/2025/0825a/"
+    },
+    {
+      name: "BUNNY BRINGS JOY",
+      desc: "三日月を運ぶウサギたちの旅をテーマにデザインしました。お気に入りのグッズと一緒にウサギたちと旅に出かけませんか。ウサギたちはあたたかさと豊かさの象徴である三日月を運ぶ旅に出ます。愛と祝福を分かち合うこの旅は、季節の恵みとやさしさをそっと広げていきます。",
+      img: "<%= image_path('goods/goods_stb_tumbler.png') %>",
+      maker: "スターバックス",
+      period: "9月3日〜なくなり次第終了",
+      tweetText: "#お月見限定商品を楽しもう #秋のミニアプリweek",
+      officialLink: "https://www.starbucks.co.jp/seasonal-goods/?srsltid=AfmBOoqtbqG9wqHipT-KSJK14OnawWwPCcGRn6JcoMSwtyEy3iStxPU_"
+    },
+  ]
+)
